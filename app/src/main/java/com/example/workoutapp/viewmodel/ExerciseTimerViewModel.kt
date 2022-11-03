e9f9f7294dbcd1799ab0c1d7cc80fe8041201e30
borisTirkajla <tirkajla.b@hotmail.com>
2022-11-03 15:46:17 +0100

    Timer updated

--- app/src/main/java/com/example/workoutapp/viewmodel/ExerciseTimerViewModel.kt
+++ app/src/main/java/com/example/workoutapp/viewmodel/ExerciseTimerViewModel.kt
@@ -7,12 +7,20 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 private const val DURATION = 6000L
-private const val COUNT_DOWN_INTERVAL = 1000L
+private const val ONE_SEC = 1000L
+private const val COUNT_DOWN_INTERVAL = 20L
 private const val TAG = "ExerciseTimerViewModel"
 
 class ExerciseTimerViewModel() : ViewModel() {
     private val _totalElapsedTime = MutableLiveData<Long>(0)
     val totalElapsedTime: LiveData<Long> = _totalElapsedTime
+
+    private var _offsetTime : Long = 0L
+
+    private var _numberOfIntervalsInOneSec: Int = 0
+    private var _numberOfIntervals: Int = 0
+
+    private var _counter: Int = 0
 
     private val _timerStarted = MutableLiveData<Boolean>(false)
     val timerStarted: LiveData<Boolean> = _timerStarted
@@ -37,21 +45,26 @@ class ExerciseTimerViewModel() : ViewModel() {
 
     fun startTimer() {
         timerExercise.start()
-        Log.d(TAG,"HERE")
+
         _isCounterRunning.value = true
-        _timerStarted.value = true
+//        _timerStarted.value = true
     }
 
     fun formatString(int: Int) {
 
-        var secString = (int % 60).toString()
-        var minString = (int / 60).toString()
+        var millisString = (int % 1000).toString()
+        var secString = (int / 1000).toString()
+        var minString = (int / 60000).toString()
 
 
-        if (secString.length == 1) secString = "0${secString.toString()}" ?: secString
-        if (minString.length == 1) minString = "0${minString.toString()}" ?: minString
+        if (millisString.length == 2) millisString = "${millisString}0"
 
-        _totalElapsedTimeString.value = "$minString:$secString"
+        if (millisString.length == 1) millisString = "${millisString}00"
+
+        if (secString.length == 1) secString = "0${secString}" ?: secString
+        if (minString.length == 1) minString = "0${minString}" ?: minString
+
+        _totalElapsedTimeString.value = "$minString:$secString:$millisString"
     }
 
     fun setNextPhotoToFalse() {
@@ -61,45 +74,56 @@ class ExerciseTimerViewModel() : ViewModel() {
     fun setDuration(duration: Long) {
         _durationInMills.value = duration * 1000
         _timeLeft.value = (duration).toInt()
+        configureTimer()
         setTimer()
     }
 
     private fun setTimer(duration: Long = _durationInMills.value!!) {
-        timerExercise = object : CountDownTimer(duration, COUNT_DOWN_INTERVAL) {
+        timerExercise = object : CountDownTimer(_durationInMills.value!!, COUNT_DOWN_INTERVAL) {
             override fun onTick(p0: Long) {
-                _totalElapsedTime.value = totalElapsedTime.value!! + 1
-                Log.d(TAG,_durationInMills.value.toString())
+//                formatString(_durationInMills.value!!.toInt() - p0.toInt())
+                _totalElapsedTime.value = (_durationInMills.value!!-p0) + _offsetTime
                 formatString(totalElapsedTime.value!!.toInt())
-                _timeLeft.value = timeLeft.value!! - 1
             }
 
             override fun onFinish() {
-                _timeLeft.value = _durationInMills.value!!.toInt() / 1000
                 _nextPhoto.value = true
+                _offsetTime = _totalElapsedTime.value!!
                 startTimer()
+//                _counter++
+//                _timeLeft.value = _timeLeft.value!!.minus(1)
+//                if (_counter == (_durationInMills.value!!/1000).toInt()) {
+//                    _nextPhoto.value = true
+//                    _timeLeft.value = (_durationInMills.value!! / 1000).toInt()
+//                    _counter = 0
+//                }
+
             }
 
         }
     }
 
+    private fun configureTimer() {
+        _numberOfIntervals = (_durationInMills.value!!.toInt() / COUNT_DOWN_INTERVAL).toInt()
+        _numberOfIntervalsInOneSec = (1000 / COUNT_DOWN_INTERVAL).toInt()
 
+    }
 
     fun resetTimer() {
         timerExercise.cancel()
-        _timerStarted.value = false
+        _offsetTime = 0
         _totalElapsedTime.value = 0
-//        _totalElapsedTimeString.value = "Boris"
-//        _durationInMills.value = 0
     }
 
     fun pauseOrResumeTimer() {
         if (_isCounterRunning.value!!) {
             _isCounterRunning.value = false
             timerExercise.cancel()
+            _offsetTime = _totalElapsedTime.value!!
         } else {
             timerExercise.start()
             _isCounterRunning.value = true
-            _timeLeft.value = (durationInMills.value)!!.toInt()/1000
+//            _timeLeft.value = (durationInMills.value)!!.toInt() / 1000
         }
     }
 
